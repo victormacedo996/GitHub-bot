8d99698f38ed9392fbdd86ea2998f512fc0bfe3a
Victor Macedo <64912176+victormacedo996@users.noreply.github.com>
2020-07-07 10:24:07 -0300

    Add files via upload

--- Github-bot.py
+++ Github-bot.py
@@ -1,9 +1,9 @@
-
 ##imports
 import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 
 ##class and function to login on Github
 class HomePage:
@@ -18,6 +18,7 @@ class HomePage:
         sing_in_button = browser.find_element_by_xpath('/html/body/div[3]/main/div/form/div[4]/input[9]')
         sing_in_button.click()
 
+
 ##class and function to create a new repository on Github
 class NewRepository:
     def create_new_repository(project_name, description, privacy):
@@ -26,7 +27,7 @@ class NewRepository:
         repository_name.send_keys(project_name)
         description_input = browser.find_element_by_xpath ('//*[@id="repository_description"]')
         description_input.send_keys(description)
-        if privacy == 'y':
+        if privacy == 's':
             mark_public = browser.find_element_by_xpath('//*[@id="repository_visibility_public"]')
             mark_public.click()
         else:
@@ -42,10 +43,11 @@ class NewRepository:
         create_repository = browser.find_element_by_xpath('/html/body/div[4]/main/div/form/div[4]/button')
         create_repository.click()
 
+
 ##class and function to create a folder on a directory and a .py file
 class CreateProject:
     def create_folder_and_file(project_name):
-        path = '<your directory>'+ project_name
+        path = 'C:\\Users\\user\\Desktop\\Python\\'+ project_name
         acess_rights = 0o755
         try:
             os.mkdir(path, acess_rights)
@@ -54,7 +56,8 @@ class CreateProject:
         else:
             print ('Successfully created the directory %s' %path)
 
-        open(path+'\\'+ project_name +'.py','w+')
+        open(path +'\\'+ project_name +'.py','w+')
+
             
 ##inputs and function calls
 browser = webdriver.Firefox()
@@ -64,7 +67,14 @@ project_name = input('What are we making today? ')
 username = input('Enter your login: ')
 password = input('Enter your password: ')
 description = input('Whats the description of the repository: ')
-privacy = input('Is the repository public? ')
+
+valid_privacy = False
+while valid_privacy == False:
+    privacy = str(input('Is the repository public? ').lower())
+    if privacy != 'y' and privacy != 'n':
+        print('Just use Y for YES and N for NO')
+    else:
+        valid_privacy = True
 
 CreateProject.create_folder_and_file(project_name)
 HomePage.login (username,password)
